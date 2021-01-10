cfb046ba50f429d89cb6da915672793ea221f92e
Peter Krokhin <p.krokhin@gmail.com>
2021-01-10 21:42:47 +0500

    Перевод на CQRS
    Промежуточный commit.

--- src/ClinicSchedule.Web/Controllers/EventGroupsController.cs
+++ src/ClinicSchedule.Web/Controllers/EventGroupsController.cs
@@ -11,18 +11,17 @@ namespace ClinicSchedule.Web
     [ApiController]
     [ApiConventionType(typeof(DefaultApiConventions))]
     [Route("[controller]")]
-    public class AppointmentsController : ControllerBase
+    public class EventGroupsController : ControllerBase
     {
         private readonly IMediator _mediator;
 
-        public AppointmentsController(IMediator mediator)
+        public EventGroupsController(IMediator mediator)
         {
             _mediator = mediator;
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<FindManyAppointmentsResponse>>> Get(int? patientId, 
-            string patientName, bool? isLinked)
+        public async Task<ActionResult<IEnumerable<FindManyEventGroups>>> Get(int? patientId, bool? isLinked, bool? getNearest)
         {
             var appointments = await _mediator.Send(new FindManyAppointmentsQuery(patientId, patientName, isLinked));
             return Ok(appointments);
